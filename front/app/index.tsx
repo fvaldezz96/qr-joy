cad47ce157395ff9df190c60d327eb500fd6c741
fvaldezz96 <valdezfede21@gmail.com>
2025-10-11 01:03:07 -0300

    fix: changes front

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,9 +1,9 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, alignItems:'center', justifyContent:'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>JoyPark está vivo 🚀</Text>
       <StatusBar style="auto" />
     </View>
