3ff38a3360c46c9d5e4e619ed746fd55bf8e6366
fvaldezz96 <valdezfede21@gmail.com>
2026-01-14 08:39:57 -0300

    fix: convert server to ESM

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const DIST_DIR = path.join(__dirname, 'dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
